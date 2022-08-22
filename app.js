670f714854fa6f263ea47278fc6ddf48cfb1b761
Narmada Muralikrishnan <71222021+letsbecometechie@users.noreply.github.com>
2022-08-22 21:03:11 +0530

    Initial commit
    Initial commit of portfolio project.

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',function(){
+    Typed.new(".dynamic-text",{
+    strings:["Developer","UI Designer","Content Creator"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+});
+});
+
